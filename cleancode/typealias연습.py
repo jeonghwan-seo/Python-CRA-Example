0e58d56c3d1d6fbdce3f44040be6cc70351d998a
JH_MINCODING <97942507+jeonghwan-seo@users.noreply.github.com>
2025-04-18 14:13:08 +0900

    Update typealias연습.py

--- cleancode/typealias연습.py
+++ cleancode/typealias연습.py
@@ -1,16 +1,16 @@
-def print_user_cart(user_id: int, items: list[tuple[str, float]]) -> None:
+def print_user_cart(user_id: int, items: list[tuple[str, int]]) -> None:
     print(f"User {user_id} 의 Shopping Cart:")
     total = 0.0
     for name, price in items:
-        print(f" - {name}: {price:.2f}원")
+        print(f" - {name}: {price}원")
         total += price
-    print(f"Total: {total:.2f}원")
+    print(f"Total: {total}원")
 
 
 cart = [
-    ("Keyboard", 49.10),
-    ("Mouse", 25.50),
-    ("Monitor", 199.10),
+    ("Keyboard", 49000),
+    ("Mouse", 25000),
+    ("Monitor", 199000),
 ]
 
 print_user_cart(1001, cart)
