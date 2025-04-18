74535b7d1fef8ff33dd16664a8bccf33f0f64fc8
JH_MINCODING <97942507+jeonghwan-seo@users.noreply.github.com>
2025-04-18 15:59:47 +0900

    Create drawmachine.py

---
+++ cleancode/drawmachine.py
@@ -0,0 +1,23 @@
+class xy_info:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class xyManager:
+    _arr = []
+
+    def add(self, data: xy_info):
+        self._arr.append(data)
+
+    def drawing(self):
+        for o in self._arr:
+            print(f"{o.x}, {o.y} 에 버튼 찍힘")
+
+
+a = xyManager()
+a.add(xy_info(1, 2))
+a.add(xy_info(2, 3))
+a.add(xy_info(5, 5))
+a.add(xy_info(7, 1))
+a.drawing()
